2cd3808beee59219eade65e114c6dc43f198e0e6
Josh Wolfer <mourne@hash.localdomain>
2022-12-15 12:49:37 -0600

    k3d working without static IP

--- post-config.sh
+++ post-config.sh
@@ -57,86 +57,86 @@ consul namespace create -name backend -partition=unicorn -http-addr="$DC1"
 consul namespace create -name frontend -partition=unicorn -http-addr="$DC2"
 consul namespace create -name backend -partition=unicorn -http-addr="$DC2"
 
-# ==========================================
-#       Register External Services
-# ==========================================
+# # ==========================================
+# #       Register External Services
+# # ==========================================
 
-# DC1/proj1/virtual-baphomet
+# # DC1/proj1/virtual-baphomet
 
-curl --request PUT --data @./configs/services-dc1-proj1-baphomet0.json --header "X-Consul-Token: root" "${DC1}/v1/catalog/register"
-curl --request PUT --data @./configs/services-dc1-proj1-baphomet1.json --header "X-Consul-Token: root" "${DC1}/v1/catalog/register"
-curl --request PUT --data @./configs/services-dc1-proj1-baphomet2.json --header "X-Consul-Token: root" "${DC1}/v1/catalog/register"
+# curl --request PUT --data @./configs/services-dc1-proj1-baphomet0.json --header "X-Consul-Token: root" "${DC1}/v1/catalog/register"
+# curl --request PUT --data @./configs/services-dc1-proj1-baphomet1.json --header "X-Consul-Token: root" "${DC1}/v1/catalog/register"
+# curl --request PUT --data @./configs/services-dc1-proj1-baphomet2.json --header "X-Consul-Token: root" "${DC1}/v1/catalog/register"
 
-# ==========================================
-#             ACLs / Auth N/Z
-# ==========================================
+# # ==========================================
+# #             ACLs / Auth N/Z
+# # ==========================================
 
-# Create ACL tokens in DC1
-consul acl token create \
-    -node-identity=client-dc1-alpha:dc1 \
-    -service-identity=joshs-obnoxiously-long-service-name-gonna-take-awhile:dc1 \
-    -service-identity=josh:dc1 \
-    -partition=default \
-    -namespace=default \
-    -secret="00000000-0000-0000-0000-000000001111" \
-    -http-addr="$DC1"
+# # Create ACL tokens in DC1
+# consul acl token create \
+#     -node-identity=client-dc1-alpha:dc1 \
+#     -service-identity=joshs-obnoxiously-long-service-name-gonna-take-awhile:dc1 \
+#     -service-identity=josh:dc1 \
+#     -partition=default \
+#     -namespace=default \
+#     -secret="00000000-0000-0000-0000-000000001111" \
+#     -http-addr="$DC1"
 
-consul acl policy create -name dc1-read -rules @./acl/dc1-read.hcl -http-addr="$DC1"
-consul acl role create -name dc1-read -policy-name dc1-read -http-addr="$DC1"
-consul acl token create \
-    -role-name=dc1-read \
-    -partition=default \
-    -namespace=default \
-    -secret="00000000-0000-0000-0000-000000003333" \
-    -http-addr="$DC1"
+# consul acl policy create -name dc1-read -rules @./acl/dc1-read.hcl -http-addr="$DC1"
+# consul acl role create -name dc1-read -policy-name dc1-read -http-addr="$DC1"
+# consul acl token create \
+#     -role-name=dc1-read \
+#     -partition=default \
+#     -namespace=default \
+#     -secret="00000000-0000-0000-0000-000000003333" \
+#     -http-addr="$DC1"
 
-# ------------------------------------------
-#          Partition proj1 RBAC
-# ------------------------------------------
+# # ------------------------------------------
+# #          Partition proj1 RBAC
+# # ------------------------------------------
 
-consul acl policy create -name team-proj1-rw -rules @./acl/team-proj1-rw.hcl -http-addr="$DC1"
-consul acl role create -name team-proj1-rw -policy-name team-proj1-rw -http-addr="$DC1"
-consul acl token create -partition=default -role-name=team-proj1-rw -secret="00000000-0000-0000-0000-000000002222" -http-addr="$DC1"
+# consul acl policy create -name team-proj1-rw -rules @./acl/team-proj1-rw.hcl -http-addr="$DC1"
+# consul acl role create -name team-proj1-rw -policy-name team-proj1-rw -http-addr="$DC1"
+# consul acl token create -partition=default -role-name=team-proj1-rw -secret="00000000-0000-0000-0000-000000002222" -http-addr="$DC1"
 
-# ------------------------------------------
-#             Consul-Admins
-# ------------------------------------------
+# # ------------------------------------------
+# #             Consul-Admins
+# # ------------------------------------------
 
-consul acl role create -name consul-admins -policy-name global-management -http-addr="$DC1"
+# consul acl role create -name consul-admins -policy-name global-management -http-addr="$DC1"
 
-# ==========================================
-#              OIDC Auth
-# ==========================================
+# # ==========================================
+# #              OIDC Auth
+# # ==========================================
 
-# Enable OIDC in Consul
+# # Enable OIDC in Consul
 
-consul acl auth-method create -type oidc \
-  -name auth0 \
-  -max-token-ttl=30m \
-  -config=@./auth/oidc-auth.json \
-  -http-addr="$DC1"
+# consul acl auth-method create -type oidc \
+#   -name auth0 \
+#   -max-token-ttl=30m \
+#   -config=@./auth/oidc-auth.json \
+#   -http-addr="$DC1"
 
-# ------------------------------------------
-# Binding rule to map Auth0 groups to Consul roles
-# ------------------------------------------
+# # ------------------------------------------
+# # Binding rule to map Auth0 groups to Consul roles
+# # ------------------------------------------
 
-# DC1/Proj1 Admins
+# # DC1/Proj1 Admins
 
-consul acl binding-rule create \
-  -method=auth0 \
-  -bind-type=role \
-  -bind-name=team-proj1-rw \
-  -selector='proj1 in list.groups' \
-  -http-addr="$DC1"
+# consul acl binding-rule create \
+#   -method=auth0 \
+#   -bind-type=role \
+#   -bind-name=team-proj1-rw \
+#   -selector='proj1 in list.groups' \
+#   -http-addr="$DC1"
 
-# DC1 Admins
+# # DC1 Admins
 
-consul acl binding-rule create \
-  -method=auth0 \
-  -bind-type=role \
-  -bind-name=consul-admins \
-  -selector='admins in list.groups' \
-  -http-addr="$DC1"
+# consul acl binding-rule create \
+#   -method=auth0 \
+#   -bind-type=role \
+#   -bind-name=consul-admins \
+#   -selector='admins in list.groups' \
+#   -http-addr="$DC1"
 
 # ==========================================
 #                JWT Auth
@@ -167,21 +167,21 @@ consul config write -http-addr="$DC2" ./configs/mgw/dc2-mgw.hcl
 
 # Peer DC1/default <> DC2/default
 
-consul peering generate-token -name dc2-default -http-addr="$DC1" > tokens/peering-dc1_default-dc2-default.token
-consul peering establish -name dc1-default -http-addr="$DC2" -peering-token $(cat tokens/peering-dc1_default-dc2-default.token)
+# consul peering generate-token -name dc2-default -http-addr="$DC1" > tokens/peering-dc1_default-dc2-default.token
+# consul peering establish -name dc1-default -http-addr="$DC2" -peering-token $(cat tokens/peering-dc1_default-dc2-default.token)
 
 # Peer DC1/default <> DC2/heimdall
 
-consul peering generate-token -name dc2-heimdall -partition="default" -http-addr="$DC1" > tokens/peering-dc1_default-dc2-heimdall.token
-consul peering establish -name dc1-default -partition="heimdall" -http-addr="$DC2" -peering-token $(cat tokens/peering-dc1_default-dc2-heimdall.token)
+# consul peering generate-token -name dc2-heimdall -partition="default" -http-addr="$DC1" > tokens/peering-dc1_default-dc2-heimdall.token
+# consul peering establish -name dc1-default -partition="heimdall" -http-addr="$DC2" -peering-token $(cat tokens/peering-dc1_default-dc2-heimdall.token)
 
 consul peering generate-token -name dc1-default -partition="chunky" -http-addr="$DC2" > tokens/peering-dc2_chunky-dc1-default.token
 consul peering establish -name dc2-chunky -partition="default" -http-addr="$DC1" -peering-token $(cat tokens/peering-dc2_chunky-dc1-default.token)
 
 # Peer DC1/unicorn <> DC2/unicorn
 
-consul peering generate-token -name dc2-unicorn -partition="unicorn" -http-addr="$DC1" > tokens/peering-dc1_unicorn-dc2-unicorn.token
-consul peering establish -name dc1-unicorn -partition="unicorn" -http-addr="$DC2" -peering-token $(cat tokens/peering-dc1_unicorn-dc2-unicorn.token)
+# consul peering generate-token -name dc2-unicorn -partition="unicorn" -http-addr="$DC1" > tokens/peering-dc1_unicorn-dc2-unicorn.token
+# consul peering establish -name dc1-unicorn -partition="unicorn" -http-addr="$DC2" -peering-token $(cat tokens/peering-dc1_unicorn-dc2-unicorn.token)
 
 # ==========================================
 #          Service Mesh Configs
