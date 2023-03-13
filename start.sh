f6e6317c80799bcc8b838457fbd0ebb103d3ab59
Josh Wolfer <mourne@hash>
2023-03-13 13:56:41 -0500

    working modular ACLs

--- start.sh
+++ start.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 # Shell colors for echo outputs
+RED='\033[1;31m'
+BLUE='\033[1;34m'
+DGRN='\033[0;32m'
 GRN='\033[1;32m'
+YELL='\033[0;33m'
 NC='\033[0m' # No Color
+
+if [[ "$*" == *"--help"* ]]
+  then
+    echo -e "Syntax: ./start.sh [OPTIONS]"
+    echo ""
+    echo "Options:"
+    echo "  -root      Launch all agents and services with root tokens"
+    echo ""
+    exit 0
+fi
 
 if [[ $(docker ps -aq) ]]; then
     echo -e "${GRN}------------------------------------------"
@@ -24,7 +38,17 @@ echo -e "        Rebuilding Doctor Consul"
 echo -e "------------------------------------------"
 echo -e "${NC}"
 
-docker-compose up
-# docker-compose up --env-file ./docker_vars/standard.env 
 
-    # image: "webapp:${TAG}"
+if [[ "$*" == *"-root"* ]]
+  then
+    echo -e "${YELL}docker-compose --env-file ./docker_vars/acl-root.env up ${NC}"
+    docker-compose --env-file docker_vars/acl-root.env up
+    echo ""
+  else
+    echo -e "${YELL}docker-compose --env-file ./docker_vars/acl-secure.env up ${NC}"
+    docker-compose --env-file docker_vars/acl-secure.env up
+    echo ""
+fi
+
+# Validated the string substitution
+# docker compose --env-file ./docker_vars/acl-root.env convert | vsc yaml
