8d9be9e7d751124ec24a864a157a330e1523e574
Josh Wolfer <mourne@gmail.com>
2023-08-15 17:42:50 -0500

    move all the docker compose environment configs to ./docker-configs/*. Much cleaner

--- post-config.sh
+++ post-config.sh
@@ -123,18 +123,10 @@ echo -e "==========================================${NC}"
 
 # DC1/proj1/virtual-baphomet
 
-echo -e ""
-echo -e "${GRN}DC1/Proj1/default/baphomet0${NC}"
-curl --request PUT --data @./configs/services/dc1-proj1-baphomet0.json --header "X-Consul-Token: root" "${DC1}/v1/catalog/register"
-
-echo -e ""
-echo -e "${GRN}DC1/Proj1/default/baphomet1${NC}"
-curl --request PUT --data @./configs/services/dc1-proj1-baphomet1.json --header "X-Consul-Token: root" "${DC1}/v1/catalog/register"
-
-echo -e ""
-echo -e "${GRN}DC1/Proj1/default/baphomet2${NC}"
-curl --request PUT --data @./configs/services/dc1-proj1-baphomet2.json --header "X-Consul-Token: root" "${DC1}/v1/catalog/register"
-
+echo ""
+echo -e "${GRN}DC1/Proj1/default/baphomet0:${NC} $(curl -s --request PUT --data @./docker-configs/configs/services/dc1-proj1-baphomet0.json --header "X-Consul-Token: root" "${DC1}/v1/catalog/register")"
+echo -e "${GRN}DC1/Proj1/default/baphomet1:${NC} $(curl -s --request PUT --data @./docker-configs/configs/services/dc1-proj1-baphomet1.json --header "X-Consul-Token: root" "${DC1}/v1/catalog/register")"
+echo -e "${GRN}DC1/Proj1/default/baphomet2:${NC} $(curl -s --request PUT --data @./docker-configs/configs/services/dc1-proj1-baphomet2.json --header "X-Consul-Token: root" "${DC1}/v1/catalog/register")"
 
   # ------------------------------------------
   #           proxy-defaults
@@ -152,16 +144,12 @@ curl --request PUT --data @./configs/services/dc1-proj1-baphomet2.json --header 
 echo -e ""
 echo -e "${GRN}proxy-defaults:${NC}"
 echo -e ""
-echo -ne "${GRN}(DC1) default Partition: ${NC}"
-consul config write -http-addr="$DC1" ./configs/proxy-defaults/dc1-default-proxydefaults.hcl
-echo -ne "${GRN}(DC1) unicorn Partition: ${NC}"
-consul config write -http-addr="$DC1" ./configs/proxy-defaults/dc1-unicorn-proxydefaults.hcl
-echo -ne "${GRN}(DC2) default Partition: ${NC}"
-consul config write -http-addr="$DC2" ./configs/proxy-defaults/dc2-default-proxydefaults.hcl
-echo -ne "${GRN}(DC2) unicorn Partition: ${NC}"
-consul config write -http-addr="$DC2" ./configs/proxy-defaults/dc2-unicorn-proxydefaults.hcl
-echo -ne "${GRN}(DC2) chunky Partition: ${NC}"
-consul config write -http-addr="$DC2" ./configs/proxy-defaults/dc2-chunky-proxydefaults.hcl
+
+echo -e "${GRN}(DC1) default Partition:${NC} $(consul config write -http-addr="$DC1" ./docker-configs/configs/proxy-defaults/dc1-default-proxydefaults.hcl)"
+echo -e "${GRN}(DC1) unicorn Partition:${NC} $(consul config write -http-addr="$DC1" ./docker-configs/configs/proxy-defaults/dc1-unicorn-proxydefaults.hcl)"
+echo -e "${GRN}(DC2) default Partition:${NC} $(consul config write -http-addr="$DC2" ./docker-configs/configs/proxy-defaults/dc2-default-proxydefaults.hcl)"
+echo -e "${GRN}(DC2) unicorn Partition:${NC} $(consul config write -http-addr="$DC2" ./docker-configs/configs/proxy-defaults/dc2-unicorn-proxydefaults.hcl)"
+echo -e "${GRN}(DC2) chunky Partition:${NC}  $(consul config write -http-addr="$DC2" ./docker-configs/configs/proxy-defaults/dc2-chunky-proxydefaults.hcl)"
 
 # ==========================================
 #             ACLs / Auth N/Z
@@ -182,7 +170,7 @@ echo -e "==========================================${NC}"
 echo -e ""
 echo -e "${GRN}Add service:read to the anonymous token (enabling DNS Service Discovery):${NC}"
 
-consul acl policy create -name dns-discovery -rules @./acl/dns-discovery.hcl -http-addr="$DC1"
+consul acl policy create -name dns-discovery -rules @./docker-configs/acl/dns-discovery.hcl -http-addr="$DC1"
 consul acl token update -id 00000000-0000-0000-0000-000000000002 -policy-name dns-discovery -http-addr="$DC1"
 
 # ------------------------------------------
@@ -195,7 +183,7 @@ echo -e "${GRN}Add DC1/unicorn cross-namespace discovery${NC}"
 consul acl policy create \
   -name "cross-namespace-sd" \
   -description "cross-namespace service discovery" \
-  -rules @./acl/cross-namespace-discovery.hcl \
+  -rules @./docker-configs/acl/cross-namespace-discovery.hcl \
   -partition=unicorn \
   -namespace=default \
   -http-addr="$DC1"
@@ -230,7 +218,7 @@ consul acl token create \
 echo -e ""
 echo -e "${GRN}ACL Policy+Role: DC1-read${NC}"
 
-consul acl policy create -name dc1-read -rules @./acl/dc1-read.hcl -http-addr="$DC1"
+consul acl policy create -name dc1-read -rules @./docker-configs/acl/dc1-read.hcl -http-addr="$DC1"
 consul acl role create -name dc1-read -policy-name dc1-read -http-addr="$DC1"
 
 echo -e ""
@@ -275,7 +263,7 @@ consul acl token create \
 echo -e ""
 echo -e "${GRN}ACL Token: 000000004444 (-policy-name=unicorn):${NC}"
 
-consul acl policy create -name unicorn -partition=unicorn -namespace=default -rules @./acl/dc1-unicorn-frontend.hcl
+consul acl policy create -name unicorn -partition=unicorn -namespace=default -rules @./docker-configs/acl/dc1-unicorn-frontend.hcl
 
 # consul acl token create \
 #     -partition=unicorn \
@@ -350,7 +338,7 @@ echo -e "------------------------------------------${NC}"
 echo -e ""
 
 echo -e "${GRN}ACL Policy+Role: DC1/proj1/team-proj1-rw${NC}"
-consul acl policy create -name team-proj1-rw -rules @./acl/team-proj1-rw.hcl -http-addr="$DC1"
+consul acl policy create -name team-proj1-rw -rules @./docker-configs/acl/team-proj1-rw.hcl -http-addr="$DC1"
 consul acl role create -name team-proj1-rw -policy-name team-proj1-rw -http-addr="$DC1"
 echo -e ""
 echo -e "${GRN}ACL Token: 000000002222${NC}"
@@ -389,7 +377,7 @@ echo -e "${GRN}Enable OIDC in Consul w/ Auth0 ${NC}"
 consul acl auth-method create -type oidc \
   -name auth0 \
   -max-token-ttl=30m \
-  -config=@./auth/oidc-auth.json \
+  -config=@./docker-configs/auth/oidc-auth.json \
   -http-addr="$DC1"
 
 # ------------------------------------------
@@ -434,7 +422,7 @@ consul acl binding-rule create \
   # consul acl auth-method create -type jwt \
   #   -name jwt \
   #   -max-token-ttl=30m \
-  #   -config=@./auth/oidc-auth.json
+  #   -config=@./docker-configs/auth/oidc-auth.json
 
   # consul acl binding-rule create \
   #   -method=auth0 \
@@ -458,8 +446,8 @@ echo -e "==========================================${NC}"
 echo -e ""
 echo -e "${GRN}Set peering to use Mesh Gateways for peering control plane traffic.${NC}"
 
-consul config write -http-addr="$DC1" ./configs/mgw/dc1-mgw.hcl
-consul config write -http-addr="$DC2" ./configs/mgw/dc2-mgw.hcl
+consul config write -http-addr="$DC1" ./docker-configs/configs/mgw/dc1-mgw.hcl
+consul config write -http-addr="$DC2" ./docker-configs/configs/mgw/dc2-mgw.hcl
 
 # Peer DC1/default <- DC2/default
 
@@ -511,16 +499,16 @@ echo -e "==========================================${NC}"
 echo -e ""
 echo -e "${GRN}service-defaults:${NC}"
 
-consul config write -http-addr="$DC1" ./configs/service-defaults/web-defaults.hcl
-consul config write -http-addr="$DC1" ./configs/service-defaults/web-upstream-defaults.hcl
-consul config write -http-addr="$DC2" ./configs/service-defaults/web-chunky-defaults.hcl
+consul config write -http-addr="$DC1" ./docker-configs/configs/service-defaults/web-defaults.hcl
+consul config write -http-addr="$DC1" ./docker-configs/configs/service-defaults/web-upstream-defaults.hcl
+consul config write -http-addr="$DC2" ./docker-configs/configs/service-defaults/web-chunky-defaults.hcl
 
       # Something funky is going on with service-defaults. Must enable the first to get static peer connections working. Can't get service-resolver to work
       # Will leave these commented out for now.
 
-consul config write -http-addr="$DC1" ./configs/service-defaults/unicorn-frontend-defaults.hcl
-consul config write -http-addr="$DC1" ./configs/service-defaults/unicorn-backend-defaults.hcl
-consul config write -http-addr="$DC2" ./configs/service-defaults/unicorn-backend-defaults.hcl
+consul config write -http-addr="$DC1" ./docker-configs/configs/service-defaults/unicorn-frontend-defaults.hcl
+consul config write -http-addr="$DC1" ./docker-configs/configs/service-defaults/unicorn-backend-defaults.hcl
+consul config write -http-addr="$DC2" ./docker-configs/configs/service-defaults/unicorn-backend-defaults.hcl
 
   # ------------------------------------------
   # Export services across Peers
@@ -533,21 +521,21 @@ echo -e "${GRN}exported-services:${NC}"
 # Multiple written configs to the same partition will stomp each other. Good times!
 
 # Export the DC1/Donkey/default/Donkey service to DC1/default/default
-consul config write -http-addr="$DC1" ./configs/exported-services/exported-services-donkey.hcl
+consul config write -http-addr="$DC1" ./docker-configs/configs/exported-services/exported-services-donkey.hcl
 
 # Export the DC1/unicorn/backend/unicorn-backend service to DC1/default/default
 # This is the only example of a mesh service being exported on a local partition
-consul config write -http-addr="$DC1" ./configs/exported-services/exported-services-dc1-unicorn.hcl
+consul config write -http-addr="$DC1" ./docker-configs/configs/exported-services/exported-services-dc1-unicorn.hcl
 
 # Export the default partition services to various peers
-consul config write -http-addr="$DC1" ./configs/exported-services/exported-services-dc1-default.hcl
-consul config write -http-addr="$DC2" ./configs/exported-services/exported-services-dc2-default.hcl
+consul config write -http-addr="$DC1" ./docker-configs/configs/exported-services/exported-services-dc1-default.hcl
+consul config write -http-addr="$DC2" ./docker-configs/configs/exported-services/exported-services-dc2-default.hcl
 
-consul config write -http-addr="$DC2" ./configs/exported-services/exported-services-dc2-webchunky.hcl
+consul config write -http-addr="$DC2" ./docker-configs/configs/exported-services/exported-services-dc2-webchunky.hcl
 
 # Export the DC1/unicorn/backend/unicorn-backend service to DC1/unicorn/backend
 
-consul config write -http-addr="$DC2" ./configs/exported-services/exported-services-dc2-unicorn_backend.hcl
+consul config write -http-addr="$DC2" ./docker-configs/configs/exported-services/exported-services-dc2-unicorn_backend.hcl
 
 
 
@@ -558,13 +546,13 @@ consul config write -http-addr="$DC2" ./configs/exported-services/exported-servi
 echo -e ""
 echo -e "${GRN}Service Intentions:${NC}"
 
-consul config write -http-addr="$DC1" ./configs/intentions/web_upstream-allow.hcl
-consul config write -http-addr="$DC2" ./configs/intentions/web_chunky-allow.hcl
+consul config write -http-addr="$DC1" ./docker-configs/configs/intentions/web_upstream-allow.hcl
+consul config write -http-addr="$DC2" ./docker-configs/configs/intentions/web_chunky-allow.hcl
 
-consul config write -http-addr="$DC1" ./configs/intentions/dc1-unicorn_frontend-allow.hcl
-consul config write -http-addr="$DC2" ./configs/intentions/dc2-unicorn_frontend-allow.hcl
+consul config write -http-addr="$DC1" ./docker-configs/configs/intentions/dc1-unicorn_frontend-allow.hcl
+consul config write -http-addr="$DC2" ./docker-configs/configs/intentions/dc2-unicorn_frontend-allow.hcl
 
-consul config write -http-addr="$DC1" ./configs/intentions/dc1-unicorn_backend_failover-allow.hcl
+consul config write -http-addr="$DC1" ./docker-configs/configs/intentions/dc1-unicorn_backend_failover-allow.hcl
 
   # ------------------------------------------
   #            Service-Resolvers
@@ -573,7 +561,7 @@ consul config write -http-addr="$DC1" ./configs/intentions/dc1-unicorn_backend_f
 echo -e ""
 echo -e "${GRN}Service-resolvers:${NC}"
 
-consul config write -http-addr="$DC1" ./configs/service-resolver/dc1-unicorn-backend-failover.hcl
+consul config write -http-addr="$DC1" ./docker-configs/configs/service-resolver/dc1-unicorn-backend-failover.hcl
 echo -e ""
 
 # ==========================================
